9722482396173e934f459f0b9edad27fb6070742
Edgar Kiyoshi Cintho <edgar@c23.local>
2018-11-09 15:17:02 -0200

    init

---
+++ config/routes.rb
@@ -0,0 +1,21 @@
+Rails.application.routes.draw do
+  resources :tasks do
+  	member do
+  		put :change
+    end
+  end
+  devise_for :users
+  get 'pages/home'
+
+  # The priority is based upon order of creatin: first created highest priority
+  # See how all your routes lay out with "rake routes"
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'pages#home'
+
+  get 'about' => "pages#about"
+
+  get 'test' => "pages#test"
+
+  #get 'removed' => "tasks#removed"
+end
